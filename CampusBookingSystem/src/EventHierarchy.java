895f5abcdfb4faf372ae141a6eb0a333f496714b
Nort999 <nhlowe09@gmail.com>
2026-03-01 16:46:41 -0500

    Modified the entire class to follow encapsulation rules

--- CampusBookingSystem/src/EventHierarchy.java
+++ CampusBookingSystem/src/EventHierarchy.java
@@ -4,25 +4,62 @@ public class EventHierarchy {
 
     // Make the default event class that include the universal event info
     static class Event {
-        int eventId;
-        String title;
-        int dateTime;
-        String location;
-        int capacity; //Must be >0
-        Boolean status;
+        private int eventId;
+        private String title;
+        private int dateTime;
+        private String location;
+        private int capacity; //Must be >0
+        private boolean status;
+
+        //Constructor
+        public Event(){}
+
+        //Getters and Setters
+        public int  getEventId() {return eventId;}
+        public void setEventId(int eventId) {this.eventId = eventId;}
+
+        public String getTitle() {return title;}
+        public void setTitle(String title) {this.title = title;}
+
+        public int getDateTime() {return dateTime;}
+        public void setDateTime(int dateTime) {this.dateTime = dateTime;}
+
+        public String getLocation() {return location;}
+        public void setLocation(String location) {this.location = location;}
+
+        public int getCapacity() {return capacity;}
+        public void setCapacity(int capacity) {
+            if(capacity <= 0){System.exit(0);}
+            this.capacity = capacity;
+        }
+
+        public boolean getStatus() {return status;}
+        public void setStatus(boolean status) {this.status = status;}
     }
 
     //Made children classes that use the Event class's variables, but also add their own
     static class Workshop extends Event {
-        String topic;
+        private String topic;
+
+        //Getters and Setters
+        public String getTopic() {return topic;}
+        public void setTopic(String topic) {this.topic = topic;}
     }
 
     static class Seminar extends Event {
-        String speakerName;
+        private String speakerName;
+
+        //Getters and Setters
+        public String getSpeakerName() {return speakerName;}
+        public void setSpeakerName(String speakerName) {this.speakerName = speakerName;}
     }
 
     static class Concert extends Event {
-        int ageRestrictment;
+        private int ageRestrictment;
+
+        //Getters and Setters
+        public int getAgeRestrictment() {return ageRestrictment;}
+        public void setAgeRestrictment(int ageRestrictment) {this.ageRestrictment = ageRestrictment;}
     }
 
     //Made a function to add an event
@@ -39,32 +76,28 @@ public class EventHierarchy {
         }
 
         IO.println("Enter ID:");
-        event.eventId = sc.nextInt();
+        event.setEventId(sc.nextInt());
         IO.println("Enter title:");
-        event.title = sc.next();
+        event.setTitle(sc.next());
         IO.println("Enter date/time:");
-        event.dateTime = sc.nextInt();
+        event.setDateTime(sc.nextInt());
         IO.println("Enter location:");
-        event.location = sc.next();
+        event.setLocation(sc.next());
         IO.println("Enter capacity:");
-        event.capacity = sc.nextInt();
-        //If capacity is not valid
-        if (event.capacity <= 0) {
-            System.exit(0); //Temporarily exits the program
-        }
+        event.setCapacity(sc.nextInt());
         // Specific fields
         switch (event) {
             case Workshop workshop -> {
                 IO.println("Enter topic:");
-                workshop.topic = sc.next();
+                workshop.setTopic(sc.next());
             }
             case Seminar seminar -> {
                 IO.println("Enter speaker name:");
-                seminar.speakerName = sc.next();
+                seminar.setSpeakerName(sc.next());
             }
             case Concert concert -> {
                 IO.println("Enter age restriction:");
-                concert.ageRestrictment = sc.nextInt();
+                concert.setAgeRestrictment(sc.nextInt());
             }
             //This needs to be here to fill all possible cases, even though it will never happen
             default -> {
@@ -72,8 +105,7 @@ public class EventHierarchy {
         }
 
         IO.println("Enter status (true/false):");
-
-        event.status = sc.nextBoolean();
+        event.setStatus(sc.nextBoolean());
 
         return event;
     }
@@ -89,41 +121,38 @@ public class EventHierarchy {
         switch (field) {
             case "ID" -> {
                 IO.println("Enter ID:");
-                event.eventId = sc.nextInt();
+                event.setEventId(sc.nextInt());
             }
             case "Title" -> {
                 IO.println("Enter title:");
-                event.title = sc.next();
+                event.setTitle(sc.next());
             }
             case "Date" -> {
                 IO.println("Enter date/time:");
-                event.dateTime = sc.nextInt();
+                event.setDateTime(sc.nextInt());
             }
             case "Location" -> {
                 IO.println("Enter location:");
-                event.location = sc.next();
+                event.setLocation(sc.next());
             }
             case "Capacity" -> {
                 IO.println("Enter capacity:");
-                event.capacity = sc.nextInt();
-                if (event.capacity <= 0) {
-                    System.exit(0);
-                }
+                event.setCapacity(sc.nextInt());
             }
             case "Topic", "Speaker", "Age" -> {
                 // Specific fields
                 switch (event) {
                     case Workshop workshop -> {
                         IO.println("Enter topic:");
-                        workshop.topic = sc.next();
+                        workshop.setTopic(sc.next());
                     }
                     case Seminar seminar -> {
                         IO.println("Enter speaker name:");
-                        seminar.speakerName = sc.next();
+                        seminar.setSpeakerName(sc.next());
                     }
                     case Concert concert -> {
                         IO.println("Enter age restriction:");
-                        concert.ageRestrictment = sc.nextInt();
+                        concert.setAgeRestrictment(sc.nextInt());
                     }
                     default -> {
                     }
@@ -139,22 +168,22 @@ public class EventHierarchy {
 
     //Sets event status to false
     void cancel(Event event) {
-        event.status = false;
+        event.setStatus(false);
     }
 
     //Prints all info of all events
 //This isn't formatted nicely
     void listEvents(ArrayList<Event> events) {
         for (Event e : events) {
-            IO.println(e.capacity);
-            IO.println(e.eventId);
-            IO.println(e.title);
-            IO.println(e.dateTime);
-            IO.println(e.location);
+            IO.println(e.getEventId());
+            IO.println(e.getTitle());
+            IO.println(e.getCapacity());
+            IO.println(e.getDateTime());
+            IO.println(e.getLocation());
             //For specific instances of the Event class
-            if (e instanceof Workshop) IO.println(((Workshop) e).topic);
-            if (e instanceof Seminar) IO.println(((Seminar) e).speakerName);
-            if (e instanceof Concert) IO.println(((Concert) e).ageRestrictment);
+            if (e instanceof Workshop) IO.println(((Workshop) e).getTopic());
+            if (e instanceof Seminar) IO.println(((Seminar) e).getSpeakerName());
+            if (e instanceof Concert) IO.println(((Concert) e).getAgeRestrictment());
         }
     }
 
@@ -170,8 +199,8 @@ public class EventHierarchy {
         //Looks through all events
         for (Event e : events) {
             //check each event in the array to see if the named on is there
-            if (name.equals(e.title)) {
-                System.out.println("Event found -- ID: " + e.eventId);
+            if (name.equals(e.getTitle())) {
+                System.out.println("Event found -- ID: " + e.getEventId());
                 break;
             }
             check++;
@@ -182,21 +211,25 @@ public class EventHierarchy {
         }
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
+        new EventHierarchy().run();
+    }
+    void run(){
         ArrayList<Event> events = new ArrayList<>();
+
+        events.add(addEvent(new Event()));
 
         events.add(addEvent(new Event()));
 
         searchEvents(events);
 
         //Sets the event at some position in the array (0 in this case) to new variables
-        events.set(0, updateEvent(events.get(0)));
+        events.set(0, updateEvent(events.getFirst()));
 
         searchEvents(events);
 
-        cancel(events.get(0));
+        cancel(events.getFirst());
 
         listEvents(events);
-
     }
 }
