cdd52d36381169017873231f607d3450cb5b71d3
Blaze1174 <Cameronsonnenburg@gmail.com>
2026-03-30 19:11:54 -0400

    commented each section for Concert class

--- CampusBookingSystem/src/model/events/Concert.java
+++ CampusBookingSystem/src/model/events/Concert.java
@@ -1,23 +1,20 @@
 package model.events;
 
-public class Concert extends Event {
-    private String ageRestriction;
+public class Concert extends Event {//subclass for event
+    private String ageRestriction; //variable for only concert class
 
-    /*
-    creates a new concert with the given details
-     */
+
+    //Constructor for concert with the given details
     public Concert(String eventId, String title, String dateTime, String location, int capacity, String status, String ageRestriction) {
-        super(eventId, title, dateTime, location, capacity, status);
+        super(eventId, title, dateTime, location, capacity, status); //super uses values from superclass event
         this.ageRestriction = ageRestriction;
     }
-
+    //getters and setters for Concert
     public String getAgeRestriction() { return ageRestriction; }
     public void setAgeRestriction(String ageRestriction) { this.ageRestriction = ageRestriction; }
 
-    /*
-    Returns the event type as a string
-     */
-
+    //Returns the event type as a string
+    //overrides replace methode from parent classes
     @Override
     public String getEventType() { return "Concert"; }
 
