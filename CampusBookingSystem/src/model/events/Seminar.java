a007a7ed60f6589a39dbcc4975561bb8f3709886
Blaze1174 <Cameronsonnenburg@gmail.com>
2026-03-30 19:15:22 -0400

    commented each section for event subclass

--- CampusBookingSystem/src/model/events/Seminar.java
+++ CampusBookingSystem/src/model/events/Seminar.java
@@ -1,19 +1,23 @@
 package model.events;
 
-public class Seminar extends Event {
-    private String speakerName;
+public class Seminar extends Event { //extends superclass Event
+    private String speakerName; //variable for only Seminar
 
+    //constructor for Seminar
     public Seminar(String eventId, String title, String dateTime, String location, int capacity, String status, String speakerName) {
-        super(eventId, title, dateTime, location, capacity, status);
+        super(eventId, title, dateTime, location, capacity, status);//super uses variables from superclass event
         this.speakerName = speakerName;
     }
 
+    //getter and setter for SpeakerName
     public String getSpeakerName() { return speakerName; }
     public void setSpeakerName(String speakerName) { this.speakerName = speakerName; }
 
+    //returns event type as string
     @Override
     public String getEventType() { return "Seminar"; }
 
+    //returns the value of Speakername
     @Override
     public String getTypeSpecificField() { return speakerName; }
 }
--- CampusBookingSystem/src/model/events/Workshop.java
+++ CampusBookingSystem/src/model/events/Workshop.java
@@ -1,19 +1,22 @@
 package model.events;
 
-public class Workshop extends Event {
-    private String topic;
+public class Workshop extends Event {//extends superclass event
+    private String topic;//workshop variable
 
+    //constructor for workshop
     public Workshop(String eventId, String title, String dateTime, String location, int capacity, String status, String topic) {
-        super(eventId, title, dateTime, location, capacity, status);
+        super(eventId, title, dateTime, location, capacity, status);//gets variables from superclass event
         this.topic = topic;
     }
-
+    //getters and setters
     public String getTopic() { return topic; }
     public void setTopic(String topic) { this.topic = topic; }
 
+    //returns eventtype as string
     @Override
     public String getEventType() { return "Workshop"; }
 
+    //returns topic as variable value
     @Override
     public String getTypeSpecificField() { return topic; }
 }
